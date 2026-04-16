80e278866f8adefda34abb3dae3f00aa4bc2dd5f
Adri Syukran Saadon <adri.syukran17@gmail.com>
2026-04-16 12:09:44 +0800

    include parse in backend

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,5 +1,6 @@
 # Backend Services Package
 
 from backend.services.llm_service import NanoGPTService, LLMResponse
+from backend.services.parser_service import ParserService, ParserError
 
-__all__ = ["NanoGPTService", "LLMResponse"]
+__all__ = ["NanoGPTService", "LLMResponse", "ParserService", "ParserError"]
